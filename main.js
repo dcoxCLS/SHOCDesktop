45e321f0c284e1c50914d10c01268f1ab62f9f5e
Bob Cowling <rcowling@mtu.edu>
2022-10-13 15:50:41 -0400

    update hamtrack

--- main.js
+++ main.js
@@ -2,25 +2,47 @@
     "esri/config",
     "esri/Map",
     "esri/views/MapView",
-
-    "esri/layers/FeatureLayer"
-
-  ], function(esriConfig,Map, MapView, FeatureLayer) {
+    "esri/layers/FeatureLayer",
+    "esri/layers/TileLayer",
+    "esri/widgets/Slider"
+  ], function(esriConfig, Map, MapView, FeatureLayer, TileLayer, Slider) {
 
   esriConfig.apiKey = "YOUR_API_KEY";
 
+  // add vector layers to the map
+
   const map = new Map({
-    basemap: "arcgis-topographic"
+    basemap: "satellite"
   });
 
   const view = new MapView({
     container: "viewDiv",
     map: map,
-    center: [-118.80543,34.02700],
-    zoom: 13
+    center: [-83.0496,42.3928],
+    zoom: 16
   });
 
-//Trailheads feature layer (points)
+  // add FIPS TileLayers to the map
+  const fips_1897 = new TileLayer({
+     url: "https://portal1-geo.sabu.mtu.edu/server/rest/services/Hosted/Sanborn_1897_Test3/MapServer"
+  });
+
+  map.add(fips_1897);
+
+  // add esri widgets
+  const slider = new Slider({
+    container: "sliderDiv",
+    layout: "vertical",
+    min: 0,
+    max: 100,
+    values: [ 100 ],
+    snapOnClickEnabled: false,
+    visibleElements: {
+      labels: false,
+      rangeLabels: true
+    }
+  });
+/*//Trailheads feature layer (points)
   const trailheadsLayer = new FeatureLayer({
     url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
   });
@@ -39,6 +61,6 @@
     url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
   });
 
-  map.add(parksLayer, 0);
+  map.add(parksLayer, 0);*/
 
   });
