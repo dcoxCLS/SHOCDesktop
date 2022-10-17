0e78835433fc65889ef1fd4a7a6b98a0eef01715
MTU Geospatial Research Facility <101424237+MTUGRF@users.noreply.github.com>
2022-10-17 11:41:01 -0400

    add opacity slider

--- main.js
+++ main.js
@@ -5,19 +5,17 @@
     "esri/layers/FeatureLayer",
     "esri/layers/TileLayer",
     "esri/widgets/Slider"
-  ], function(esriConfig, Map, MapView, FeatureLayer, TileLayer, Slider) {
-
-  esriConfig.apiKey = "YOUR_API_KEY";
+  ], function(esriConfig, Map, MapView, FeatureLayer, TileLayer, Slider) {  
 
   // Create a style for the chartsLayer
   const renderer = {
     type: "simple",  // autocasts as new SimpleRenderer()
     symbol: {
       type: "simple-fill",  // autocasts as new SimpleFillSymbol()
-      color: [ 255, 128, 0, 1],
+      color: [ 255, 0, 0],
       outline: {  // autocasts as new SimpleLineSymbol()
         width: 2,
-        color: "gray"
+        color: "black"
       }
     }
   }; 
@@ -30,7 +28,7 @@
 
   const fips_1897 = new TileLayer({
      url: "https://portal1-geo.sabu.mtu.edu/server/rest/services/Hosted/Sanborn_1897_5/MapServer",
-     visible: true
+     visible: false
   });
 
   const fips_1915 = new TileLayer({
@@ -45,7 +43,7 @@
   
  const fips_49_51 = new TileLayer({
      url: "https://portal1-geo.sabu.mtu.edu/server/rest/services/Hosted/Sanborn_1949_1951/MapServer",
-     visible: false
+     visible: true
   });
   
   // Add the excavation sites layer to the map   
@@ -63,9 +61,16 @@
   const view = new MapView({
     container: "viewDiv",
     map: map,
-    center: [-83.0496,42.3928],
-    zoom: 16
-  });  
+    center: [-83.052756,42.387895],
+    zoom: 20
+  });
+
+  // set the default opacity of the sanborn layers
+  atlas_1885.opacity = 100;  
+  fips_1897.opacity = 100;
+  fips_1910.opacity = 100;
+  fips_1915.opacity = 100;
+  fips_49_51.opacity = 100; 
 
   //map.add(fips_1897);
 
@@ -80,6 +85,52 @@
     visibleElements: {
       labels: false,
       rangeLabels: true
+    }
+  });
+
+  // Listen for changes on the opacity slider
+  slider.on(['thumb-change', 'thumb-drag'], function(event) {
+  atlas_1885.opacity = event.value / 100;  
+  fips_1897.opacity = event.value / 100;
+  fips_1910.opacity = event.value / 100;
+  fips_1915.opacity = event.value / 100;
+  fips_49_51.opacity = event.value / 100;   
+});
+
+  // Code for the location dropdown menu
+  $("#location").change(function () {
+    // Get the value of the selected item
+    var value = this.value;
+    if (value == '1885') {
+      atlas_1885.visible = true;
+      fips_1897.visible = false;
+      fips_1915.visible = false;
+      fips_1910.visible = false;
+      fips_49_51.visible = false;              
+    } else if (value == '1897') {
+      atlas_1885.visible = false;
+      fips_1897.visible = true;
+      fips_1915.visible = false;
+      fips_1910.visible = false;
+      fips_49_51.visible = false;       
+    } else if (value == '1910') {
+      atlas_1885.visible = false;
+      fips_1897.visible = false;
+      fips_1915.visible = false;
+      fips_1910.visible = true;
+      fips_49_51.visible = false;  
+    } else if (value == '1915') {            
+      atlas_1885.visible = false;
+      fips_1897.visible = false;
+      fips_1915.visible = true;
+      fips_1910.visible = false;
+      fips_49_51.visible = false;  
+    } else if (value == '1949') {            
+      atlas_1885.visible = false;
+      fips_1897.visible = false;
+      fips_1915.visible = false;
+      fips_1910.visible = false;
+      fips_49_51.visible = true;  
     }
   });
 
